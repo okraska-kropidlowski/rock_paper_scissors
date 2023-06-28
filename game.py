e0cbc57853e65a98bdad4876f83fc0992f038338
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-06-28 22:26:36 +0200

    Result evaluation added

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 available_choice = ['R', 'P', 'S', 'r', 'p', 's', 'Rock', 'Paper', 'Scissors', 'rock', 'paper', 'scissors']
 while True:
@@ -7,12 +8,37 @@ while True:
         break
 if user_choice == 'R' or user_choice == 'r' or user_choice == 'Rock' or user_choice == 'rock':
     attack = 'ROCK'
-    if user_choice == 'P' or user_choice == 'p' or user_choice == 'Paper' or user_choice == 'paper':
-        attack = 'PAPER'
-    if user_choice == 'S' or user_choice == 's' or user_choice == 'Scissors' or user_choice == 'scissors':
-        attack = 'SCISSORS'
-print("You go with",attack + ".")
+if user_choice == 'P' or user_choice == 'p' or user_choice == 'Paper' or user_choice == 'paper':
+    attack = 'PAPER'
+if user_choice == 'S' or user_choice == 's' or user_choice == 'Scissors' or user_choice == 'scissors':
+    attack = 'SCISSORS'
+print("You go with", attack + ".")
 
 cpu_choice = ['ROCK', 'PAPER', 'SCISSORS']
 defense = random.choice(cpu_choice)
-print("I go with",defense + ".")
+print("I go with", defense + ".")
+sleep(1)
+
+if attack == 'ROCK':
+    if defense == 'ROCK': 
+        print("We have a draw...")
+    if defense == 'PAPER':
+        print('I won!')
+    if defense == 'SCISSORS':
+        print('You won... THIS TIME!')
+
+if attack == 'PAPER':
+    if defense == 'PAPER': 
+        print("We have a draw...")
+    if defense == 'SCISSORS':
+        print('I won!')
+    if defense == 'ROCK':
+        print('You won... THIS TIME!')
+
+if attack == 'SCISSORS':
+    if defense == 'SCISSORS': 
+        print("We have a draw...")
+    if defense == 'ROCK':
+        print('I won!')
+    if defense == 'PAPER':
+        print('You won... THIS TIME!')
