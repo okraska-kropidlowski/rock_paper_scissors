b15b3351712b3f056e0d8d10a87ca5dd61942dfe
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-07-03 13:40:02 +0200

    Add the round result part of the GUI

--- gui_game.py
+++ gui_game.py
@@ -3,7 +3,7 @@ import random
 
 window = tkinter.Tk()
 window.title("Rock-Paper-Scissors")
-window.geometry("640x480")
+window.geometry("800x600")
 window.resizable(False, False)
 
 frame = tkinter.Frame(window)
@@ -11,6 +11,7 @@ frame.pack()
 
 defense = ""
 cpu_choice = ["✊", "✋", "✌️"]
+result = ""
 
 def cpu_move():
     global defense
@@ -18,24 +19,42 @@ def cpu_move():
     defense = random.choice(cpu_choice)
     cpu_choice_ui.config(text=defense)
 
-user_gesture_ui = tkinter.LabelFrame(frame, text="Choose your gesture:", font=("Arial", 12))
-user_gesture_ui.grid(row=0, column=0, padx=20, pady=20)
+user_gesture_ui = tkinter.LabelFrame(frame, text="Choose your gesture:")
+user_gesture_ui.grid(row=0)
 
 rock_button = tkinter.Button(user_gesture_ui, text="✊")
-rock_button.grid(row=0, column=0)
+rock_button.grid(column=0)
 paper_button = tkinter.Button(user_gesture_ui, text="✋")
-paper_button.grid(row=0, column=1)
+paper_button.grid(column=1)
 scissors_button = tkinter.Button(user_gesture_ui, text="✌️")
-scissors_button.grid(row=0, column=2)
+scissors_button.grid(column=2)
 
-for widget in user_gesture_ui.winfo_children():
-    widget.grid_configure(padx=10, pady=10)
-    widget.config(font=("Arial", 50), highlightthickness=0, bd=0, command=cpu_move)
+round_result_ui = tkinter.LabelFrame(frame, text="Round result:")
+round_result_ui.grid(row=1)
 
-cpu_gesture_ui = tkinter.LabelFrame(frame, text="CPU gesture:", font=("Arial", 12))
-cpu_gesture_ui.grid(row=1, column=0, padx=20, pady=20)
+result_ui = tkinter.Label(round_result_ui, text=result)
+result_ui.grid(column=0)
 
-cpu_choice_ui = tkinter.Label(cpu_gesture_ui, text="", font=("Arial", 50))
-cpu_choice_ui.grid(row=1, column=0, padx=10, pady=10)
+cpu_gesture_ui = tkinter.LabelFrame(frame, text="CPU gesture:")
+cpu_gesture_ui.grid(row=2)
+
+cpu_choice_ui = tkinter.Label(cpu_gesture_ui, text="")
+cpu_choice_ui.grid(column=0)
+
+for widget_labelframe in frame.winfo_children():
+    widget_labelframe.grid_configure(column=0, padx=20, pady=20)
+    widget_labelframe.config(font=("Arial", 12), width=10)
+
+for widget_button in user_gesture_ui.winfo_children():
+    widget_button.grid_configure(row=0, padx=10, pady=10)
+    widget_button.config(font=("Arial", 50), highlightthickness=0, bd=0, command=cpu_move)
+
+for widget_label_result in round_result_ui.winfo_children():
+    widget_label_result.grid_configure(row=1, padx=20, pady=20)
+    widget_label_result.config(font=("Arial", 50), width=10, justify='center')
+
+for widget_label_cpu in cpu_gesture_ui.winfo_children():
+    widget_label_cpu.grid_configure(row=1, padx=20, pady=20)
+    widget_label_cpu.config(font=("Arial", 50), width=10, justify='center')
 
 window.mainloop()
